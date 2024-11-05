da79a7fe0fc44e35c52f07010653c75a3e2bdd5a
baeyongwoo <dyddn30612@naver.com>
2024-11-05 15:02:09 +0900

    찜기능 완료 쪄서 버려버리고 싶네

--- devel/src/main/java/com/treasuredigger/devel/controller/WishlistController.java
+++ devel/src/main/java/com/treasuredigger/devel/controller/WishlistController.java
@@ -1,11 +1,17 @@
 package com.treasuredigger.devel.controller;
 
 import com.treasuredigger.devel.dto.BidItemDto;
+import com.treasuredigger.devel.dto.ItemDto;
+import com.treasuredigger.devel.dto.ItemFormDto;
 import com.treasuredigger.devel.entity.BidItem;
+import com.treasuredigger.devel.entity.Item;
 import com.treasuredigger.devel.entity.Member;
+import com.treasuredigger.devel.repository.ItemRepository;
 import com.treasuredigger.devel.service.BidItemService;
+import com.treasuredigger.devel.service.ItemService;
 import com.treasuredigger.devel.service.MemberService;
 import com.treasuredigger.devel.service.WishlistService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,28 +29,69 @@ public class WishlistController {
     private MemberService memberService;
 
     @Autowired
+    private ItemService itemService;
+
+    @Autowired
     private BidItemService bidItemService;
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     @GetMapping("/check")
-    public ResponseEntity<Boolean> checkWishlist(@RequestParam("bidItemId") String bidItemId, Principal principal) {
-        String email  = principal.getName();
+    public ResponseEntity<Boolean> checkWishlist(@RequestParam("entityType") String entityType,
+                                                 @RequestParam("entityId") String entityId,
+                                                 Principal principal) {
+        String email = principal.getName();
         Member member = memberService.findMemberByMid(email);
-        BidItemDto bidItemDto =  bidItemService.viewDtl(bidItemId);
 
-        BidItem bidItem = bidItemDto.convertDtoToEntity(bidItemDto);
-        System.out.print("email data + "+  email +"bidItem Id " + bidItemId + "member " + member + "bidItemDto" + bidItemDto);
+        boolean isWishlisted = false;
+        try {
+            if ("biditem".equalsIgnoreCase(entityType)) {
+                BidItemDto bidItemDto = bidItemService.viewDtl(entityId);
+                BidItem bidItem = bidItemDto.convertDtoToEntity(bidItemDto);
+                isWishlisted = wishlistService.isWishlisted(bidItem, member);
+            } else if ("item".equalsIgnoreCase(entityType)) {
+                Long itemId = Long.parseLong(entityId);
+                Item item = itemRepository.findById(Long.valueOf(entityId))
+                        .orElseThrow(EntityNotFoundException::new);
 
-        boolean isWishlisted = wishlistService.isWishlisted(bidItem, member);
+                System.out.println("check +++ " + item);
+
+                isWishlisted = wishlistService.isWishlisted(item, member);
+            } else {
+                return ResponseEntity.badRequest().body(false);
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(false);
+        }
+
         return ResponseEntity.ok(isWishlisted);
     }
 
     @PostMapping("/toggle")
-    public ResponseEntity<String> toggleWishlist(@RequestParam("bidItemId") String bidItemId, Principal principal) {
-        String email  = principal.getName();
+    public ResponseEntity<String> toggleWishlist(@RequestParam("entityId") String entityId,
+                                                 @RequestParam("entityType") String entityType,
+                                                 Principal principal) {
+        String email = principal.getName();
         Member member = memberService.findMemberByMid(email);
-        BidItemDto bidItemDto =  bidItemService.viewDtl(bidItemId);
-        BidItem bidItem = bidItemDto.convertDtoToEntity(bidItemDto);
-        wishlistService.toggleWishlist(bidItem, member);
+
+        try {
+            if ("biditem".equalsIgnoreCase(entityType)) {
+                BidItemDto bidItemDto = bidItemService.viewDtl(entityId);
+                BidItem bidItem = bidItemDto.convertDtoToEntity(bidItemDto);
+                wishlistService.toggleWishlist(bidItem, member);
+            } else if ("item".equalsIgnoreCase(entityType)) {
+
+                Item item = itemRepository.findById(Long.valueOf(entityId))
+                        .orElseThrow(EntityNotFoundException::new);
+                wishlistService.toggleWishlist(item, member);
+            } else {
+                return ResponseEntity.badRequest().body("Invalid entityType");
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Invalid entityId format");
+        }
+
         return ResponseEntity.ok("Success");
     }
 }
