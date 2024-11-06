7992e406d0e4db83b6672b9d86ea5898bdf6749f
kjh <kimjh9068@naver.com>
2024-11-06 18:19:28 +0900

    결제가 되는데 돈이어떻게 될지 모르므로 결제는 진짜로 하지마세요

--- devel/src/main/java/com/treasuredigger/devel/dto/PaymentDto.java
+++ devel/src/main/java/com/treasuredigger/devel/dto/PaymentDto.java
@@ -2,23 +2,27 @@ package com.treasuredigger.devel.dto;
 
 import com.treasuredigger.devel.constant.PaymentStatus;
 import com.treasuredigger.devel.entity.Order;
-import com.treasuredigger.devel.entity.Payment;
+import com.treasuredigger.devel.entity.PaymentEntity;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class PaymentDto {
-    Long paymentId;
-    Order order;
-    String impUid;
-    String merchantUid;
-    int amount;
-    PaymentStatus status;
-    LocalDateTime paidAt;
 
-    public Payment toEntity() {
-        return Payment.builder()
+    private Long paymentId;
+    private Long memberId;  // 회원 정보
+    private Long productId; // 상품 정보
+    private Long orderId;   // 주문 정보
+    private String impUid;  // 아임포트 고유 ID
+    private String merchantUid;  // 가맹점 주문 ID
+    private int amount;  // 결제 금액
+    private Order order;
+    private PaymentStatus status;  // 결제 상태
+    private LocalDateTime paidAt;  // 결제 완료 시간
+
+    public PaymentEntity toEntity() {
+        return PaymentEntity.builder()
                 .id(paymentId)
                 .order(order)
                 .impUid(impUid)
@@ -29,3 +33,4 @@ public class PaymentDto {
                 .build();
     }
 }
+
